fc63b1224bd00cd85f5c39ca78eec9ea36939a87
fblogy <39649744+fblogy@users.noreply.github.com>
2019-05-09 17:04:11 +0800

    Update prime.cpp

--- Math/prime.cpp
+++ Math/prime.cpp
@@ -20,14 +20,17 @@ void getprime() {
 const int N = 3e7 + 6;
 const int M = 2e6 + 6;
 //int low[N],
-bitset<N> isp;
+bitset<N / 3 + 1> isp;
 int cntp,p[M];
  
-void getprime() {
+void getprime(int N) {
 	cntp = 2;p[0] = 2;p[1] = 3;
-	for (int i = 5, k = 1; i <= N; (k & 1) == 1 ? i+=2 : i+=4 , k++){
-		if (!isp[k]) p[cntp++]=i;
-		for (int j = 2; j < cntp && p[j] * i < N; j++) {
+	for (int i = 5, k = 1; i <= N; (k & 1) ? i+=2 : i+=4 , k++){
+		if (!isp[k]) {
+			p[cntp++]=i;
+			// low[i] = i;
+		}
+		for (int j = 2; j < cntp && p[j] * i <= N; j++) {
 			//low[p[j] * i] = p[j];
 			isp[p[j] * i / 3] = 1;
 			if (i % p[j] == 0) break;
@@ -44,13 +47,13 @@ bitset<N / 3 + 1> bit;
 void getprime(int n){
 	int i, j;
 	cntp = 2; p[0] = 2; p[1] = 3;
-	for(i = 5, j = 1; i * i <= n; (j & 1) == 1 ? i += 2 : i += 4 , j++) {
+	for(i = 5, j = 1; i * i <= n; (j & 1) ? i += 2 : i += 4 , j++) {
 		if(bit[j] == 0) {
 			p[cntp++]=i;
 			for(int j = i * i; j <= n ; j += i) 
 				if(j % 2 != 0 && j % 3 != 0) bit[j / 3] = 1;
 		}
 	}
-	for( ; i <= n; (j&1)==1 ? i+=2 : i+=4 , j++) if(bit[j] == 0) p[cntp++]=i;
+	for( ; i <= n; (j&1) ? i+=2 : i+=4 , j++) if(bit[j] == 0) p[cntp++]=i;
 }
 
